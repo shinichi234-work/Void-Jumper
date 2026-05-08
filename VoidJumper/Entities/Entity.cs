be2146164fdf3b4ba5fd99faafbf4244e59d3df5
Fleynqq <fleyn007@gmail.com>
2026-05-09 01:14:25 +0300

    Add xUnit tests for Entity, Player and LevelBuilder

--- VoidJumper/Entities/Entity.cs
+++ VoidJumper/Entities/Entity.cs
@@ -4,4 +4,11 @@ public class Entity
 {
     public string Name { get; set; } = "";
     public int Health { get; set; }
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health < 0)
+            Health = 0;
+    }
 }
