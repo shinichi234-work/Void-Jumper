35407d2fc5d784af1f636973ef7bd43316584048
shinichi234-work <1334shinichi@gmail.com>
2026-05-20 15:22:13 +0300

    Remove debug output, polish menu and add README

--- VoidJumper/Systems/RangedAttack.cs
+++ VoidJumper/Systems/RangedAttack.cs
@@ -7,10 +7,6 @@ public class RangedAttack : ICombatStrategy
     public void Execute(Enemy enemy)
     {
         if (enemy.MaxHealth > 0 && (double)enemy.Health / enemy.MaxHealth < 0.2)
-        {
             enemy.SetStrategy(new FleeBehavior());
-            return;
-        }
-        Console.WriteLine($"{enemy.Name} fires from a distance for {enemy.Damage} damage");
     }
 }
